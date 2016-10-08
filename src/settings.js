b55421ad40c2b09dd918c1759f13873338bd346e
Alejandro Blanco <alejandro.b.e@gmail.com>
2016-10-08 20:40:10 +0200

    Restore dummy bot token

--- src/settings.js
+++ src/settings.js
@@ -2,8 +2,7 @@
 // MIT License
 
 const settings = {
-    token: '287339343:AAEaU_pH_OKypX_PFX5AiIodAGLJRGp8V24'
-    // token: 'YOUR_TELEGRAM_BOT_TOKEN'
+    token: 'YOUR_TELEGRAM_BOT_TOKEN'
 };
 
 module.exports = settings;
